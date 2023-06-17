5ef5bd694addce11711b70acec3504487e64a94a
Cüneyt Kaya <53535761+kayacuneyd@users.noreply.github.com>
2023-06-17 16:21:06 +0000

    media edding

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -3,25 +3,56 @@ import "../App.css";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-//import { Route, BrowserRouter as Router } from "react-router-dom";
 
-const App = () => {
+const BlogList = () => {
   const [posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const postsPerPage = 10; // Define the desired number of posts per page
+  const visiblePageRange = 5; // Define the desired number of visible pagination buttons
+  const halfRange = Math.floor(visiblePageRange / 2);
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
         const response = await axios.get(
-          `https://buyutechaber.com/wp-json/wp/v2/posts`
+          `https://buyutechaber.com/wp-json/wp/v2/posts?page=${currentPage}&per_page=${postsPerPage}`
         );
         setPosts(response.data);
+        setTotalPages(response.headers["x-wp-totalpages"]);
       } catch (error) {
         console.error(error);
       }
     };
 
     fetchPosts();
-  }, []);
+  }, [currentPage, postsPerPage]);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const getPaginationRange = () => {
+    let startPage = currentPage - halfRange;
+    let endPage = currentPage + halfRange;
+
+    if (startPage < 1) {
+      startPage = 1;
+      endPage = visiblePageRange;
+    }
+
+    if (endPage > totalPages) {
+      endPage = totalPages;
+      startPage = totalPages - visiblePageRange + 1;
+      if (startPage < 1) {
+        startPage = 1;
+      }
+    }
+
+    return Array.from({ length: endPage - startPage + 1 }, (_, i) => startPage + i);
+  };
+
+  const paginationRange = getPaginationRange();
 
   return (
     <div className="App">
@@ -35,8 +66,20 @@ const App = () => {
           <p dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}></p>
         </div>
       ))}
+
+      <div className="pagination">
+        {paginationRange.map((page) => (
+          <button
+            key={page}
+            onClick={() => handlePageChange(page)}
+            disabled={page === currentPage}
+          >
+            {page}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
 
-export default App;
+export default BlogList;
